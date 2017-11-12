6ac8d6d8b352b75a7a58ece9350d65d837732132
sururami <30791890+sururami@users.noreply.github.com>
2017-11-12 19:05:11 +0900

    Update udpClientExample.py

--- UDP/udpClientExample.py
+++ UDP/udpClientExample.py
@@ -6,7 +6,7 @@ sock.bind(('', 0))
 
 while True:
 	data = input()
-	sock.sendto(data.encode(), ('127.0.0.1', 10000)) # ServerIP로 변경
+	sock.sendto(data.encode(), ('127.0.0.1', 10000)) #실제 Server IP로 변경
 
 	data, addr = sock.recvfrom(65535)
 	print(data.decode())
