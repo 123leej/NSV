8b163e4a47f83ef1b715010aee9c2d42120b9cbb
sururami <30791890+sururami@users.noreply.github.com>
2017-11-12 18:59:22 +0900

    Add files via upload

---
+++ UDP/udpClientExample.py
@@ -0,0 +1,12 @@
+﻿import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(('', 0))
+
+while True:
+	data = input()
+	sock.sendto(data.encode(), ('192.168.43.171', 10000))
+
+	data, addr = sock.recvfrom(65535)
+	print(data.decode())
---
+++ UDP/udpServerExample.py
@@ -0,0 +1,12 @@
+﻿import socket
+
+sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+
+# Server
+sock.bind(('192.168.43.171', 10000))
+print("listen: ", 10000)
+while True:
+	data, addr = sock.recvfrom(65535)
+	print("ip: ", addr[0], "port: ", addr[1])
+	print("echo data: ", data.decode())
+	sock.sendto(data, addr)
