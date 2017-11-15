c780cd3c1f721edb2cdbd30af816ce1420a41305
leesangjin <vitamin8096@naver.com>
2017-11-16 00:13:37 +0900

    mod/tcp&udp

--- test/UDP/udpClientExample.py
+++ test/UDP/udpClientExample.py
@@ -1,6 +1,22 @@
 ﻿import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 0))
+
+while True:
+	init_data = "a"
+	sock.sendto(str(init_data).encode(), ('127.0.0.1', 8000))
+	data, addr = sock.recvfrom(65535)
+
+# recive
+# print(data.decode())
+
+
+
+
+
+'''
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(('', 0))
 
@@ -10,3 +26,4 @@ while True:
 
 	data, addr = sock.recvfrom(65535)
 	print(data.decode())
+'''
