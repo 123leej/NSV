128527be1d3f4e6af35bfcc92f247902945ed9ca
jongwon <123leej@naver.com>
2017-11-16 03:11:39 +0900

    add/simulator_frame

--- NSV.py
+++ NSV.py
@@ -1,3 +1,4 @@
+from Simulator import Simulator
 from GUI.NSV_First_Dialog import NSVUi
 from NODE.AccessNode import run_node_proc
 
@@ -17,6 +18,7 @@ if __name__ == "__main__":
         algorithm_file_path = params["file_path"]
         number_of_node = params["number_of_nodes"]
         nodes_init_data = run_node_proc(number_of_node)
+        Simulator()
 
     if selected_menu(len(params)) is "Performance_Analysis":
         result_data_path = params["file_path"]
--- Simulator.py
+++ Simulator.py
@@ -1,11 +1,22 @@
+import sys
+from PyQt5 import QtWidgets
+from GUI.NSV_Sync_Window import SimulatorUi
 
 # TODO UPDATE NODES : receive data format ([[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],.....] )
 # TODO UPDATE NODES : set Node number as ObjectName, draw Nodes by x, y  agnet_list = [num1, num2] -> receive_data[num1], receive_data[num2] set as a bigger circle?
+# TODO UPDATE NODES : this function must have udp server function to get data from udp client(Algorithm)
 
-class Simulator():
+
+class Simulator:
     def __init__(self):
+        app = QtWidgets.QApplication(sys.argv)
+        window = QtWidgets.QDialog()
+        ui = SimulatorUi()
+        ui.setup_ui(window)
+        window.show()
+        sys.exit(app.exec_())
 
+    def update_nodes(self):
 
-
-
-
+# for test - it will run in NSV
+Simulator()
