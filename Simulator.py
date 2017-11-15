8728ad15cd6653a1b82ff38db85a6bcacf4a2c1f
jongwon <123leej@naver.com>
2017-11-16 02:38:19 +0900

    add/simmulator_class

---
+++ Simulator.py
@@ -0,0 +1,11 @@
+
+# TODO UPDATE NODES : receive data format ([[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],.....] )
+# TODO UPDATE NODES : set Node number as ObjectName, draw Nodes by x, y  agnet_list = [num1, num2] -> receive_data[num1], receive_data[num2] set as a bigger circle?
+
+class Simulator():
+    def __init__(self):
+
+
+
+
+
