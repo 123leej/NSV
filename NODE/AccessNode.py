313046d545769661c6d1eabf9644adc5dc07748f
jongwon <123leej@naver.com>
2017-11-16 01:21:07 +0900

    mod/fix bug in run node proc

--- NODE/AccessNode.py
+++ NODE/AccessNode.py
@@ -3,8 +3,9 @@ import subprocess
 
 def run_node_proc(nodes):
     nodes_data = {}
-    for i in range(1, nodes+1):
+    for i in range(1, int(nodes)+1):
         node_run_port = subprocess.check_output("python NODE/node.py " + str(i), shell=True)
         nodes_data['node'+str(i)] = {"port": node_run_port}
+        print(i)
 
     return nodes_data
