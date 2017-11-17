ad6cc214dfd6394bf58d9b5e3ee1768819e2d4b6
jongwon <123leej@naver.com>
2017-11-17 18:23:33 +0900

    fix/bug_with_log_managements

--- Simulator.py
+++ Simulator.py
@@ -9,10 +9,6 @@ from GUI.NSV_Sync_Window import SimulatorUi
 from util.RunProcess import run_process
 from util.KillProcess import kill_process
 
-# TODO UPDATE NODES : receive data format ([[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],[node_idx, x, y, len_from_agentA, len_from_agentB],.....] )
-# TODO UPDATE NODES : set Node number as ObjectName, draw Nodes by x, y  agnet_list = [num1, num2] -> receive_data[num1], receive_data[num2] set as a bigger circle?
-# TODO UPDATE NODES : this function must have udp server function to get data from udp client(Algorithm)
-
 
 class Simulator:
     def __init__(self):
@@ -20,6 +16,7 @@ class Simulator:
         self.agent_B = None
         self.thread_list = []
         self.log_file = []
+        self.log_file_buffer = []
 
         app = QtWidgets.QApplication(sys.argv)
         window = QtWidgets.QDialog()
@@ -33,6 +30,14 @@ class Simulator:
             self.open_log_file(node_number+1)
             self.thread_list[node_number] = threading.Thread(target=self.run_node, args=node_number+1)
             self.thread_list[node_number].start()
+
+    def run_algorithm(self, _file, _node):
+        for idx, data in enumerate(run_process(_file + " " + _node)):
+            if idx is not 0 :
+                # TODO Edit after drawing nodes objects
+            else:
+                self.agent_A = data[0]
+                self.agent_A = data[1]
 
     def stop_algorithm(self, _file):
         non_extension = os.path.splitext(_file)[0]
@@ -48,30 +53,21 @@ class Simulator:
                 self.write_log(_node_number, log.decode('utf-8'))
 
     def open_log_file(self, _node_number):
-        self.log_file[_node_number] = open("./log/node" + _node_number + "_log.txt", "a")
+        self.log_file.append("node" + str(_node_number) + "_log.txt")
+        self.log_file_buffer.append(open("./log/" + self.log_file[_node_number], "a"))
 
     def write_log(self, _node_number, _log):
         try:
-            self.log_file[_node_number].write(_log + "\n")
+            self.log_file_buffer[_node_number].write(_log + "\n")
         except ValueError:
-            exit(0)
+            self.stop_all_simulation()
+            # TODO Edit after stop_all_simulation
 
     def save_logs(self):
-        for i in range(0, len(self.log_file)):
-            self.log_file[i].close()
+        for i in range(0, len(self.log_file_buffer)):
+            self.log_file_buffer[i].close()
 
-    def merge_log_files(self):
-        self.log_file
-
-    def run_algorithm(self, _file, _node):
-        for idx, data in enumerate(run_process(_file + " " + _node)):
-            if idx is not 0 :
-                self.draw_data(data)
-            else:
-                self.agent_A = data[0]
-                self.agent_A = data[1]
-
-
+        self.log_file_buffer = None
 
     def merge_log_files(self):
         with open("./log/"+datetime.datetime.now().strftime('%Y-%m-%d')+"_simulation", "w") as result_file:
